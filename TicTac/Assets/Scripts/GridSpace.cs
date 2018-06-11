eada0c1b6007abebcddaca115d2170dc3483ed67
Andrea <fruitypebbles1@hotmail.com>
2018-06-11 00:59:13 -0700

    correct files

---
+++ TicTac/Assets/Scripts/GridSpace.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class GridSpace : MonoBehaviour
+{
+
+    public Button button;
+    public Text buttonText;
+  
+
+    private GameController gameController;
+
+
+    public void SetSpace()
+    {
+        if (gameController.playerMove == true)
+        {
+            buttonText.text = gameController.GetPlayerSide();
+            button.interactable = false;
+            gameController.EndTurn();
+
+        }    
+    }
+
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
+    }
+
+
+}
+
+
+
